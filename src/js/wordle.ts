dccf064f3bd4a27fbfe9eb5fbfebda2716907f3a
Dex <35111165+Lamby777@users.noreply.github.com>
2022-05-21 23:19:29 +0000

    Added timer code

--- src/js/wordle.ts
+++ src/js/wordle.ts
@@ -8,10 +8,14 @@ const frame		= <HTMLDivElement>
 	document.getElementById("cellFrame");
 const inputBox	= <HTMLInputElement>
 	document.getElementById("inputBox");
+const timerE	= <HTMLHeadingElement>
+	document.getElementById("timer");
 
 // Main function, as async to allow await
 (async () => {
-	const words = await retrieveWords();
+	const WORDS_LIST = await retrieveWords();
+	let gameRunning = false;
+	let startTime: number;
 	
 	// Make each row
 	for (let i = 0; i < VISIBLE_ROWS; i++) {
@@ -42,43 +46,54 @@ const inputBox	= <HTMLInputElement>
 	inputBox.addEventListener("keydown", (e) => {
 		if (e.key === "Enter") {
 			attemptSubmit();
+
+			if (!gameRunning) {
+				gameRunning = true;
+				startTime = Date.now();
+				setInterval(timerTick, 40);
+			}
 		}
 	});
-})();
 
-function filterInput(premod: string): string {
-	const caps = premod.toUpperCase();
-	let truncated = caps;
-	if (truncated.length > COLUMNS)
-		truncated = truncated.substring(0, COLUMNS);
-
-	return truncated;
-}
-
-function attemptSubmit() {
-	const guess = inputBox.value;
-
-	// Don't allow guesses with less letters
-	if (guess.length !== COLUMNS) return fail();
-
-	function fail(col: string = "red"): void {
-		inputBox.style.background = col;
-		
-		setTimeout(() => {
-			inputBox.style.background = "white";
-		}, 300);
+	function filterInput(premod: string): string {
+		const caps = premod.toUpperCase();
+		let truncated = caps;
+		if (truncated.length > COLUMNS)
+			truncated = truncated.substring(0, COLUMNS);
+	
+		return truncated;
 	}
-}
+	
+	function attemptSubmit() {
+		const guess = inputBox.value;
+	
+		// Don't allow guesses with less letters
+		if (guess.length !== COLUMNS) return fail("red");
+
+		// Don't allow guesses from made-up words
+		if (!(WORDS_LIST.includes(guess))) return fail("darkred");
+	
+		function fail(col: string = "red"): void {
+			inputBox.style.background = col;
+			
+			setTimeout(() => {
+				inputBox.style.background = "white";
+			}, 300);
+		}
+	}
+
+	async function timerTick() {
+		const elapsed = Date.now() - startTime;
+		const left = 90 - (elapsed / 1000);
+		const dispLeft = left < 0 ? 0 : Math.ceil(left);
+		
+		timerE.innerText = dispLeft + "s";
+	}
+})();
 
 async function retrieveWords() {
 	// Request data from server
-	let res =
-		await fetch("/words.txt")
+	return	await fetch("/words.txt")
 				.then(data => data.text())
-				.then(str => str.split(/\s+/));
-
-	console.log(res);
-	
-	// Parse into array
-	return res;
+				.then(str => str.toUpperCase().split(/\s+/));
 }
