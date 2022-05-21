d24d92e3fa3c27504d0b8858195a8270f754ce3e
Dex <35111165+Lamby777@users.noreply.github.com>
2022-05-21 22:20:53 +0000

    Wordle words added

--- src/js/wordle.ts
+++ src/js/wordle.ts
@@ -1,31 +1,71 @@
 "use strict";
 
-const VISIBLE_ROWS = 2;
+const VISIBLE_ROWS	= 2;
+const COLUMNS		= 6;
 
-const frame = <HTMLDivElement> document.getElementById("cellFrame");
+// Get elements as TypeScript typecasted values
+const frame		= <HTMLDivElement>
+	document.getElementById("cellFrame");
+const inputBox	= <HTMLInputElement>
+	document.getElementById("inputBox");
 
-// Make each row
-for (let i = 0; i < VISIBLE_ROWS; i++) {
-	const row = document.createElement("div");
-	row.classList.add("row");
-
-	// Make cells in row
-	for (let j = 0; j < 6; j++) {
-		const cell = document.createElement("div");
-		cell.classList.add("cell", "col-2");
-
-		//const ctc = document.createElement("div");
-		//ctc.classList.add("cellTextContainer");
+// Main function, as async to allow await
+(async () => {
+	const words = await fetch("/words.txt");
+	
+	// Make each row
+	for (let i = 0; i < VISIBLE_ROWS; i++) {
+		const row = document.createElement("div");
+		row.classList.add("row");
+	
+		// Make cells in row
+		for (let j = 0; j < COLUMNS; j++) {
+			const cell = document.createElement("div");
+			cell.classList.add("cell", "col-2");
+	
+			const cellText = document.createElement("div");
+			cellText.classList.add("cellText");
+			cellText.innerHTML = "X";
+			
+			cell.appendChild(cellText);
+			row.appendChild(cell);
+		}
 		
-		const cellText = document.createElement("div");
-		cellText.classList.add("cellText");
-		cellText.innerHTML = "X";
-
-		//ctc.appendChild(cellText);
-		//cell.appendChild(ctc);
-		cell.appendChild(cellText);
-		row.appendChild(cell);
+		frame.appendChild(row);
 	}
 	
-	frame.appendChild(row);
+	// Filter input box
+	inputBox.addEventListener("input", (e) => {
+		inputBox.value = filterInput(inputBox.value);
+	});
+	
+	inputBox.addEventListener("keydown", (e) => {
+		if (e.key === "Enter") {
+			attemptSubmit();
+		}
+	});
+});
+
+function filterInput(premod: string): string {
+	const caps = premod.toUpperCase();
+	let truncated = caps;
+	if (truncated.length > COLUMNS)
+		truncated = truncated.substring(0, COLUMNS);
+
+	return truncated;
+}
+
+function attemptSubmit() {
+	const guess = inputBox.value;
+
+	// Don't allow guesses with less letters
+	if (guess.length !== COLUMNS) return fail();
+
+	function fail(col: string = "red"): void {
+		inputBox.style.background = col;
+		
+		setTimeout(() => {
+			inputBox.style.background = "white";
+		}, 300);
+	}
 }
