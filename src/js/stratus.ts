67d7f0029dcc921a2616960c8dc64adb8d62e3de
Dex <sheepareawesome777@gmail.com>
2023-03-01 06:28:00 -0500

    Check for login in Stratus + Rm useless email box on login page
    You were supposed to leave it blank anyway... Might as well comment it out til it serves a purpose later, right?

--- src/js/stratus.ts
+++ src/js/stratus.ts
@@ -1,2 +1,21 @@
 // Stratus client-side code
 "use strict";
+
+import {str, cmon, elem}	from "libdx";
+import {io}					from "socket.io-client";
+import {Modal, Carousel}	from "bootstrap";
+const socket = io();
+
+const nagModalE	= document.getElementById("loginNagModal")!;
+const nagModal	= new Modal(nagModalE);
+
+if (!currentAccount) {
+	nagModal.show();
+	nagModalE.addEventListener("hidden.bs.modal", () => {
+		window.location.href = "/login";
+	});
+}
+
+socket.on("connect", () => {
+	console.log("Connected to Socket.IO");
+});
