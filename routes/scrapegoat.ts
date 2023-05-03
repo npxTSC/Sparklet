46d12a993c73650d1f1ccb5620ba8d6aa0c338aa
Dex <sheepareawesome777@gmail.com>
2023-05-03 01:21:16 -0400

    Looks like scraping HTML isn't enough...

--- routes/scrapegoat.ts
+++ routes/scrapegoat.ts
@@ -1,7 +1,13 @@
 "use strict";
 
-import Express	from "express";
-import cheerio	from "cheerio";
+import Express			from "express";
+import cheerio			from "cheerio";
+import puppeteer		from "puppeteer-extra";
+import StealthPlugin	from "puppeteer-extra-plugin-stealth";
+
+const CHROME_EXEC_PATH = "/home/mira/chrome-linux/chrome";
+
+puppeteer.use(StealthPlugin());
 
 const MC_API_URL 			= "https://api.mojang.com/users/profiles/minecraft/";
 const NAMEMC_PROFILE_PAGE	= "https://namemc.com/profile/";
@@ -15,7 +21,7 @@ router.get("/namemc", async (req, res) => {
 		return res.json({error: "what in sh33p's name are ya up to? :P"})
 	}
 
-	const uuid = await (await fetch(MC_API_URL + username)).json();
+	const uuid = (await (await fetch(MC_API_URL + username)).json())["id"];
 
 	return res.json({
 		nameHistory: await scrapeNameHistory(uuid)
@@ -23,27 +29,49 @@ router.get("/namemc", async (req, res) => {
 });
 
 async function scrapeNameHistory(uuid: string) {
-	const namemcProfile = await (await fetch(NAMEMC_PROFILE_PAGE + uuid)).text();
-	console.log(namemcProfile);
-	const $ = cheerio.load(namemcProfile);
-
-	// the big boi container "card mb-3"
-	const bigContainer = $(`span:contains("Name History")`).parent().parent();
-
-	// the place where all the <tr>s are dumped into ;-;
-	const tbody = bigContainer.find(".card-body").find("table").find("tbody");
-	const trElements = tbody.children("tr");
+	const browser = await puppeteer.launch({
+		headless: false,
+		executablePath: CHROME_EXEC_PATH,
+		ignoreDefaultArgs: ['--enable-automation'],
+	});
 
 	const res: [string, string][] = [];
+	
+	try {
+		const page = await browser.newPage();
+		console.log(NAMEMC_PROFILE_PAGE);
+		console.log(uuid);
+		console.log(NAMEMC_PROFILE_PAGE + uuid);
+		await page.goto(NAMEMC_PROFILE_PAGE + uuid, {waitUntil: "domcontentloaded"});
+		await new Promise(resolve => setTimeout(resolve, 15000));
 
-	console.log({
-		f: bigContainer,
-	})
+		
+		const namemcProfile = await page.content();
+		console.log(namemcProfile);
 
-	for (let i = 0; i < trElements.length; i += 2) {
-		const el = trElements[i];
-		const name = el.type;
-		res.push([name, name]);
+
+
+		const $ = cheerio.load(namemcProfile);
+
+		// the big boi container "card mb-3"
+		const bigContainer = $(`span:contains("Name History")`).parent().parent();
+
+		// the place where all the <tr>s are dumped into ;-;
+		const tbody = bigContainer.find(".card-body").find("table").find("tbody");
+		const trElements = tbody.children("tr");
+
+		console.log({
+			f: bigContainer,
+		})
+
+		for (let i = 0; i < trElements.length; i += 2) {
+			const el = trElements[i];
+			const name = el.type;
+			res.push([name, name]);
+		}
+	} finally {
+		// don't forget to close, even if errors out
+		await browser.close();
 	}
 
 	return res;
