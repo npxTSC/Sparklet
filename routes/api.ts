9a61cdb3ec26c948791265a955304410db127fd0
Dex <sheepareawesome777@gmail.com>
2023-05-04 22:08:47 -0400

    bok bok bok (chicken wings update)

--- routes/api.ts
+++ routes/api.ts
@@ -4,6 +4,7 @@ import Express		from "express";
 import statements	from "../statements";
 
 const router = Express.Router();
+const CHICKEN_WINGS_URL = "https://i.imgur.com/95Awa6y.png";
 
 router.get("/capsules", (req, res) => {
 	let rows;
@@ -23,8 +24,14 @@ router.get("/capsules", (req, res) => {
 router.get("/tea-capes", (req, res) => {
 	// TODO admin portal for changing capes
 	res.json({
-		"4772c57f-ca43-440c-be84-d5a97b676792":
-			"https://user-images.githubusercontent.com/10100202/203159684-08e62b4d-3b63-4f39-a4fc-21fc4960f346.png"
+		"Mira_Serenade":						CHICKEN_WINGS_URL,
+		"4772c57f-ca43-440c-be84-d5a97b676792":	CHICKEN_WINGS_URL,
+
+		"Epsilon094":							CHICKEN_WINGS_URL,
+		"8a7809c5-8351-4322-8f3f-6b5abce09c81":	CHICKEN_WINGS_URL,
+
+		"III_zP0_III":							CHICKEN_WINGS_URL,
+		"8d5fdde6-9ae9-480b-9cf2-30167933a10e":	CHICKEN_WINGS_URL,
 	})
 });
 
