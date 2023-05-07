16b68305b0426e223b1098c732013d3fe21d2153
Dex <sheepareawesome777@gmail.com>
2023-05-07 13:50:47 -0400

    Added `adminCpl` router, moved `/lmfao` to `/cpl/wipe-users`

--- routes/all.ts
+++ routes/all.ts
@@ -4,3 +4,4 @@ export {default as rooms}		from "./rooms.js";
 export {default as wk}			from "./wk.js";
 export {default as auth}		from "./auth.js";
 export {default as api}			from "./api.js";
+export {default as adminCpl}	from "./adminCpl.js";
