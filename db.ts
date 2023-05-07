fd32632db003ef3c52ffab01a17bd214f4f32ed5
Dex <sheepareawesome777@gmail.com>
2023-05-06 20:24:33 -0400

    Finished statement encapsulation + Changed db structure to include primary keys

--- db.ts
+++ db.ts
@@ -16,55 +16,7 @@ const conn = await mysql.createConnection({
 	password:	process.env["MYSQL_ROOT_PASSWORD"],
 });
 
-{
-	conn.execute(`
-		CREATE TABLE IF NOT EXISTS users(
-			name			TEXT		NOT NULL,
-			uuid			TEXT		NOT NULL,
-			passHash		TEXT		NOT	NULL,
-			date			DATETIME	NOT NULL DEFAULT CURRENT_TIMESTAMP,
-			adminRank		INTEGER		NOT NULL DEFAULT 0,
-			emailVerified	BOOLEAN		NOT NULL DEFAULT 0,
-			emailVToken		TEXT,
-			authToken		TEXT,
-			bio				TEXT
-		);
-	`);
-
-	conn.execute(`
-		CREATE TABLE IF NOT EXISTS news(
-			title		TEXT		NOT NULL,
-			author		TEXT		NOT NULL DEFAULT 'Anonymous',
-			content		TEXT		NOT NULL,
-			visible		BOOLEAN		NOT NULL DEFAULT 1,
-			date		DATETIME	NOT NULL DEFAULT CURRENT_TIMESTAMP
-		);
-	`);
-
-	conn.execute(`
-		CREATE TABLE IF NOT EXISTS games(
-			id			TEXT		PRIMARY KEY,
-			title		TEXT		NOT NULL,
-			creator		TEXT		NOT NULL DEFAULT 'Anonymous',
-			description	TEXT		NOT NULL DEFAULT 'No description given... :(',
-			visible		BOOLEAN		NOT NULL DEFAULT 1,
-			date		DATETIME	NOT NULL DEFAULT CURRENT_TIMESTAMP
-		);
-	`);
-
-	conn.execute(`
-		CREATE TABLE IF NOT EXISTS capsules(
-			uuid		TEXT		PRIMARY KEY,
-			name		TEXT		NOT NULL,
-			creator		TEXT		NOT NULL,
-			version		TEXT		NOT NULL,
-			content		TEXT		NOT NULL,
-			visible		BOOLEAN		NOT NULL DEFAULT 1,
-			date		DATETIME	NOT NULL DEFAULT CURRENT_TIMESTAMP,
-			likes		INTEGER		NOT NULL DEFAULT 0
-		);
-	`);
-}
+initTables(conn);
 
 [//	Reserved Username	// Reason
 	"Dexie",			// Admin account
@@ -136,10 +88,6 @@ export namespace db {
 		`, [bio, user]);
 	}
 
-
-
-
-	
 	export async function editLoginToken(user: string, newToken: string) {
 		return await conn.execute(`
 			UPDATE users
@@ -148,15 +96,25 @@ export namespace db {
 		`, [newToken, user]);
 	}
 
-	export const getUser = conn.execute(`
-		SELECT name, uuid, adminRank, bio FROM users
-		WHERE name = ? COLLATE NOCASE;
-	`);
+	export async function getUser(username: string) {
+		return await conn.execute(`
+			SELECT name, uuid, adminRank, bio FROM users
+			WHERE name = ? COLLATE NOCASE;
+		`, [username]);
+	}
 
-	export const postCapsule = conn.execute(`
-		INSERT INTO capsules(uuid, name, creator, version, content)
-		VALUES (?, ?, ?, ?, ?);
-	`);
+	export async function postCapsule(
+		uuid:		string,
+		name:		string,
+		creator:	string,
+		version:	string,
+		content:	string
+	) {
+		return await conn.execute(`
+			INSERT INTO capsules(uuid, name, creator, version, content)
+			VALUES (?, ?, ?, ?, ?);
+		`);
+	}
 
 	export async function getCapsule(capsuleUuid: string) {
 		return await conn.execute(`
@@ -165,43 +123,106 @@ export namespace db {
 		`, [capsuleUuid]);
 	}
 
-	export const searchCapsules = conn.execute(`
-		SELECT rowid, *
-		FROM capsules WHERE visible = 1 AND name like '%' || ? || '%'
-		ORDER BY rowid DESC
-		LIMIT 25;
-	`);
+	export async function searchCapsules(query: string) {
+		return await conn.execute(`
+			SELECT rowid, *
+			FROM capsules WHERE visible = 1 AND name like '%' || ? || '%'
+			ORDER BY rowid DESC
+			LIMIT 25;
+		`, [query])
+	}
 
-	export const capsuleQPosts = conn.execute(`
-		SELECT rowid, *
-		FROM capsules WHERE visible = 1
-		ORDER BY likes DESC
-		LIMIT 25;
-	`);
+	export async function capsuleQPosts() {
+		return await conn.execute(`
+			SELECT rowid, *
+			FROM capsules WHERE visible = 1
+			ORDER BY likes DESC
+			LIMIT 25;
+		`);
+	}
 
-	export const getGame = conn.execute(`
-		SELECT rowid, * FROM games
-		WHERE id = (?) AND visible = 1;
-	`);
+	export async function getGame(uuid: string) {
+		return await conn.execute(`
+			SELECT rowid, * FROM games
+			WHERE uuid = (?) AND visible = 1;
+		`, [uuid]);
+	}
 
-	export const gameQPosts = conn.execute(`
-		SELECT rowid, *
-		FROM games WHERE visible = 1
-		ORDER BY rowid DESC
-		LIMIT 25;
-	`);
+	export async function gameQPosts() {
+		return await conn.execute(`
+			SELECT rowid, *
+			FROM games WHERE visible = 1
+			ORDER BY rowid DESC
+			LIMIT 25;
+		`);
+	}
 
-	export const getNews = conn.execute(`
-		SELECT rowid, * FROM news
-		WHERE rowid = (?) AND visible = 1;
-	`);
+	export async function getNews(uuid: string) {
+		return await conn.execute(`
+			SELECT rowid, * FROM news
+			WHERE uuid = (?) AND visible = 1;
+		`, [uuid]);
+	}
 
-	export const newsQPosts = conn.execute(`
-		SELECT title, author, date, rowid
-		FROM news WHERE visible = 1
-		ORDER BY rowid DESC
-		LIMIT 25;
-	`);
+	export async function newsQPosts() {
+		return await conn.execute(`
+			SELECT title, author, date, rowid
+			FROM news WHERE visible = 1
+			ORDER BY rowid DESC
+			LIMIT 25;
+		`);
+	}
 }
 
 export default db;
+
+function initTables(conn: mysql.Connection) {
+	conn.execute(`
+		CREATE TABLE IF NOT EXISTS users(
+			uuid			TEXT		PRIMARY KEY,
+			name			TEXT		NOT NULL,
+			passHash		TEXT		NOT	NULL,
+			date			DATETIME	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			adminRank		INTEGER		NOT NULL DEFAULT 0,
+			emailVerified	BOOLEAN		NOT NULL DEFAULT 0,
+			emailVToken		TEXT,
+			authToken		TEXT,
+			bio				TEXT,
+		);
+	`);
+
+	conn.execute(`
+		CREATE TABLE IF NOT EXISTS news(
+			uuid		TEXT		PRIMARY KEY,
+			title		TEXT		NOT NULL,
+			author		TEXT		NOT NULL DEFAULT 'Anonymous',
+			content		TEXT		NOT NULL,
+			visible		BOOLEAN		NOT NULL DEFAULT 1,
+			date		DATETIME	NOT NULL DEFAULT CURRENT_TIMESTAMP
+		);
+	`);
+
+	conn.execute(`
+		CREATE TABLE IF NOT EXISTS games(
+			uuid		TEXT		PRIMARY KEY,
+			title		TEXT		NOT NULL,
+			creator		TEXT		NOT NULL DEFAULT 'Anonymous',
+			description	TEXT		NOT NULL DEFAULT 'No description given... :(',
+			visible		BOOLEAN		NOT NULL DEFAULT 1,
+			date		DATETIME	NOT NULL DEFAULT CURRENT_TIMESTAMP
+		);
+	`);
+
+	conn.execute(`
+		CREATE TABLE IF NOT EXISTS capsules(
+			uuid		TEXT		PRIMARY KEY,
+			name		TEXT		NOT NULL,
+			creator		TEXT		NOT NULL,
+			version		TEXT		NOT NULL,
+			content		TEXT		NOT NULL,
+			visible		BOOLEAN		NOT NULL DEFAULT 1,
+			date		DATETIME	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			likes		INTEGER		NOT NULL DEFAULT 0
+		);
+	`);
+}
