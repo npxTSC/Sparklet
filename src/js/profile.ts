de26b33a4c0c376b5a86806be622f4839ce3d8a0
Dex <sheepareawesome777@gmail.com>
2023-05-17 14:25:30 -0400

    Load profile info into TS for profiles page

--- src/js/profile.ts
+++ src/js/profile.ts
@@ -3,3 +3,10 @@
 * Used for stuff like editing your own profile
 */
 "use strict";
+
+import {SparkletDB} from "../../classes.js";
+
+const profileInfo = (window as any).profileInfo as SparkletDB.SparkletUser;
+
+const ownProfile = profileInfo.uuid === currentAccount.uuid;
+console.log(`This is ${ownProfile ? "" : "not "}your profile.`);
