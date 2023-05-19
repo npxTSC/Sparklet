932b70cb307b878146f9dcc491f0125354c4fe3a
Dex <sheepareawesome777@gmail.com>
2023-05-19 11:01:47 -0400

    are you serious

--- consts.ts
+++ consts.ts
@@ -1,7 +1,6 @@
 "use strict";
 
 import { AdminRank }	from "./classes.js";
-import path				from "path";
 
 export const PORT				= 3000;
 export const MAX_FILE_UPLOAD_MB	= 200;
@@ -9,8 +8,6 @@ export const QP_NAME_LIMIT		= 20;
 export const BIO_CHAR_LIMIT		= 400;
 export const GITHUB_PAGE		= "https://github.com/Lamby777/SparkletX";
 export const AUTH_TOKEN_BITS	= 256;
-export const STATEFUL			= path.resolve("/var/lib/sparklet");
-export const sparksFolder		= path.resolve(STATEFUL, "sparks");
 
 export const ADMINS				= {
 	"Anonymous":	AdminRank.Conductor,	// Reserved for default name
