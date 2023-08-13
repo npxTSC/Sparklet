8031665d2cc8526de0b12891c866d3a645169a9f
Cherry <cherry@sparklet.org>
2023-08-12 21:10:25 -0400

    init rs project + move old `src` -> `assets`

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// the start of something great...?
+// aka something that will probably never be completely finished!
+// but then again... all great things take time
+
+use libdx::libdx;
+
+fn main() {
+    println!("Hello, world!");
+}
