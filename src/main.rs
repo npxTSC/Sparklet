42a3619f2a1bf6344e287409fa32b395dc2018b2
Cherry <cherry@sparklet.org>
2023-08-15 02:03:23 -0400

    hello world stuff (finally got DNS to work aaaaaaaaaaa)

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,28 @@
 // aka something that will probably never be completely finished!
 // but then again... all great things take time
 
-use libdx::*;
+// use libdx::*;
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
-fn main() -> Result<()> {
-    println!("Hello, world!");
-
-    Ok(())
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+    .bind(("127.0.0.1", 5001))?
+    .run()
+    .await
 }
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
