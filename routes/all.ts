4516ce683bd06464ccdf33da01e5c382af4e5350
Cherry <cherry@sparklet.org>
2023-09-11 02:25:05 -0400

    rm old backend

--- routes/all.ts
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-export { default as rooms } from "./rooms.js";
-export { default as wk } from "./wk.js";
-export { default as auth } from "./auth.js";
-export { default as api } from "./api.js";
-export { default as adminCpl } from "./adminCpl.js";
