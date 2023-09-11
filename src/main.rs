756324c64e997c29117e30c3d200a605dd9624e3
Cherry <cherry@sparklet.org>
2023-09-11 15:28:13 -0400

    eslint init

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,15 @@ use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-    })
-    .bind(("127.0.0.1", 5001))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello).service(echo))
+        .bind(("127.0.0.1", 5001))?
+        .run()
+        .await
 }
 
 #[get("/")]
 async fn hello() -> impl Responder {
+    println!("Test");
     HttpResponse::Ok().body("Hello world!")
 }
 
@@ -26,4 +23,3 @@ async fn hello() -> impl Responder {
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
-
