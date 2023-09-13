242bed5861b135fe4924906233d03654515ac2c3
Cherry <cherry@sparklet.org>
2023-09-13 17:01:04 -0400

    run script

--- src/api.rs
+++ src/api.rs
@@ -29,7 +29,6 @@ async fn tea_capes() -> impl Responder {
 fn read_capes_json<P: AsRef<Path>>(path: P) -> Result<Value> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
-    let capes = serde_json::from_reader(reader)?;
-    Ok(capes)
+    serde_json::from_reader(reader)
 }
 */
