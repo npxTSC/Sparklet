feac9ef9a5814eca8f7f0e910dede6425815362d
Cherry <cherry@sparklet.org>
2023-09-13 14:09:41 -0400

    serve /assets

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,26 @@ use std::path::PathBuf;
 
 const FRONTEND_DIR: &str = "./vue-app";
 const SERVE_DIR: &str = concatcp!(FRONTEND_DIR, "/dist");
+const ASSETS_DIR: &str = concatcp!(SERVE_DIR, "/assets");
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(index).service(echo).service(
-            Files::new("/static", SERVE_DIR)
-                .show_files_listing()
-                .use_last_modified(true),
-        )
+        App::new()
+            // .service(index)
+            .route("/", web::get().to(index))
+            // .service(echo)
+            .service(Files::new("/assets", ASSETS_DIR))
     })
     .bind(("127.0.0.1", 5001))?
     .run()
     .await
 }
 
-#[get("/")]
-async fn index(req: HttpRequest) -> Result<NamedFile> {
-    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+// #[get("/")]
+async fn index(_req: HttpRequest) -> Result<NamedFile> {
+    let path = concatcp!(SERVE_DIR, "/index.html");
+    let path = PathBuf::from(path);
     Ok(NamedFile::open(path)?)
 }
 
