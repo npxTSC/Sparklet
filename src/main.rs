b3ec0591314f64c11296959179f91a53a8dae8a2
Cherry <cherry@sparklet.org>
2023-09-12 23:33:30 -0400

    serve index/statics

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,32 @@
 // aka something that will probably never be completely finished!
 // but then again... all great things take time
 
-// use libdx::*;
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use actix_files::{Files, NamedFile};
+use actix_web::*;
+use const_format::concatcp;
+use std::path::PathBuf;
 
 const FRONTEND_DIR: &str = "./vue-app";
+const SERVE_DIR: &str = concatcp!(FRONTEND_DIR, "/dist");
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(echo))
-        .bind(("127.0.0.1", 5001))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new().service(index).service(echo).service(
+            Files::new("/static", SERVE_DIR)
+                .show_files_listing()
+                .use_last_modified(true),
+        )
+    })
+    .bind(("127.0.0.1", 5001))?
+    .run()
+    .await
 }
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    println!("Test");
-    HttpResponse::Ok().body("Hello world!")
+async fn index(req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(NamedFile::open(path)?)
 }
 
 #[post("/echo")]
