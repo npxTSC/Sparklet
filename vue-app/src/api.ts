873a801485525140e271f88c10e13d5b25b70d62
Cherry <cherry@sparklet.org>
2023-10-08 07:12:13 -0400

    add neoconf + sync

---
+++ vue-app/src/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const USE_LOCAL_API = false; // process.env.NODE_ENV === "development";
+
+const API_BASE_URL = USE_LOCAL_API
+  ? "http://localhost:5001/api"
+  : "https://sparklet.org/api";
+
+const client = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  // Define your API functions here
+  async getUsers() {
+    return (await client.get("/"));
+  },
+};
--- vue-app/src/main.ts
+++ vue-app/src/main.ts
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// const data = fetch()
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
