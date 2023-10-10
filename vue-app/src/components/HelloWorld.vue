795d24ab8ecfc2e9a4dca888c7c89f5ecb0ae552
Cherry <cherry@sparklet.org>
2023-10-09 22:13:47 -0400

    use Less

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+// defineProps<{
+//   msg: string
+// }>()
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">Among Us</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      Deez Nuts
     </h3>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="less">
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
