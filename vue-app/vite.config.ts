ee31e327ea39907cc8c512154602578967398c7e
Cherry <cherry@sparklet.org>
2024-04-06 23:58:46 -0400

    add mpa plugin

--- vue-app/vite.config.ts
+++ vue-app/vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
+import mpa from 'vite-plugin-mpa';
 
 import { resolve } from "path";
 import { defineConfig } from "vite";
@@ -6,22 +7,23 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
-      },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+                nested: resolve(__dirname, "nested/index.html"),
+            },
+        },
     },
-  },
 
-  plugins: [
-    vue(),
-  ],
+    plugins: [
+        vue(),
+        mpa(),
+    ],
 
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
 });
