359fe43f6f2b99293c71d22e7b63e385e5801884
Cherry <cherry@sparklet.org>
2024-07-19 00:25:13 -0400

    add account route

--- src/api.rs
+++ src/api.rs
@@ -3,11 +3,15 @@ use actix_web::*;
 use std::path::PathBuf;
 
 pub fn router(cfg: &mut web::ServiceConfig) {
-    // Register the /hello route
     cfg.route("/tea-capes", web::get().to(tea_capes));
+    cfg.route("/account", web::get().to(account));
 }
 
 async fn tea_capes() -> impl Responder {
     let path = PathBuf::from("./tea-capes.json");
     NamedFile::open(path)
+}
+
+async fn account() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
 }
