9088abdf303dc5afdc537cfc2bf13838d7688a86
Cherry <cherry@sparklet.org>
2024-07-18 22:24:00 -0400

    page renders as html, not text

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@
 // aka something that will probably never be completely finished!
 // but then again... all great things take time
 
-use actix_files::Files;
 use actix_web::*;
 use const_format::concatcp;
+use http::StatusCode;
 use sailfish::TemplateOnce;
 
 mod api;
@@ -23,8 +23,6 @@ async fn main() -> std::io::Result<()> {
                 cfg.service(web::scope("/api").configure(api::router));
             })
             .service(index)
-            .service(Files::new("/", SERVE_DIR))
-            .service(Files::new("/assets", ASSETS_DIR))
     })
     .bind(("127.0.0.1", 5001))?
     .run()
@@ -37,5 +35,7 @@ async fn index(_req: HttpRequest) -> impl Responder {
         messages: vec![String::from("foo"), String::from("bar")],
     };
 
-    ctx.render_once().unwrap()
+    HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(ctx.render_once().unwrap())
 }
