ef6dd2193db717d70d6b517e2a840733af9db677
Cherry <cherry@sparklet.org>
2024-07-19 18:16:21 -0400

    make tea cape local (again)

--- routes/api.ts
+++ routes/api.ts
@@ -6,53 +6,53 @@ import { AdminRank, SparkletDB } from "../classes.js";
 import { bioFilter } from "../feutils.js";
 
 const router = Express.Router();
-const CHICKEN_WINGS_URL = "https://i.imgur.com/95Awa6y.png";
+const CHICKEN_WINGS_URL = "https://sparklet.org/public/tea-cape.png";
 
 router.get("/capsules", (req, res) => {
-  let rows;
+    let rows;
 
-  if (req.query.q) {
-    const spaced = (<string> req.query.q).replace(/\+/g, " ");
-    const searchQuery = decodeURIComponent(spaced);
-    rows = db.searchCapsules(searchQuery);
-  } else {
-    rows = db.capsuleQPosts();
-  }
+    if (req.query.q) {
+        const spaced = (<string>req.query.q).replace(/\+/g, " ");
+        const searchQuery = decodeURIComponent(spaced);
+        rows = db.searchCapsules(searchQuery);
+    } else {
+        rows = db.capsuleQPosts();
+    }
 
-  return res.status(200).json(rows);
+    return res.status(200).json(rows);
 });
 
 router.get("/tea-capes", (_, res) => {
-  // TODO admin portal for changing capes
-  res.json({
-    "Mira_Serenade": CHICKEN_WINGS_URL,
-    "4772c57f-ca43-440c-be84-d5a97b676792": CHICKEN_WINGS_URL,
+    // TODO admin portal for changing capes
+    res.json({
+        "Mira_Serenade": CHICKEN_WINGS_URL,
+        "4772c57f-ca43-440c-be84-d5a97b676792": CHICKEN_WINGS_URL,
 
-    "Epsilon094": CHICKEN_WINGS_URL,
-    "8a7809c5-8351-4322-8f3f-6b5abce09c81": CHICKEN_WINGS_URL,
+        "Epsilon094": CHICKEN_WINGS_URL,
+        "8a7809c5-8351-4322-8f3f-6b5abce09c81": CHICKEN_WINGS_URL,
 
-    "III_zP0_III": CHICKEN_WINGS_URL,
-    "8d5fdde6-9ae9-480b-9cf2-30167933a10e": CHICKEN_WINGS_URL,
-  });
+        "III_zP0_III": CHICKEN_WINGS_URL,
+        "8d5fdde6-9ae9-480b-9cf2-30167933a10e": CHICKEN_WINGS_URL,
+    });
 });
 
 router.post("/profile-mod/bio", (req, res) => {
-  const { newBio, uuid: targetUUID } = req.body;
+    const { newBio, uuid: targetUUID } = req.body;
 
-  if (res.locals.account === null || !newBio) {
-    return res.status(400).end();
-  }
+    if (res.locals.account === null || !newBio) {
+        return res.status(400).end();
+    }
 
-  const acc = res.locals.account as SparkletDB.SparkletUser;
+    const acc = res.locals.account as SparkletDB.SparkletUser;
 
-  if (acc.uuid !== targetUUID && acc.adminRank < AdminRank.Manager) {
-    // must be Manager+ to edit bios of other users
-    return res.status(403).end();
-  }
+    if (acc.uuid !== targetUUID && acc.adminRank < AdminRank.Manager) {
+        // must be Manager+ to edit bios of other users
+        return res.status(403).end();
+    }
 
-  db.updateBio(targetUUID, bioFilter(newBio));
+    db.updateBio(targetUUID, bioFilter(newBio));
 
-  return res.status(200).end();
+    return res.status(200).end();
 });
 
 export default router;
