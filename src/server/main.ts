a22743f92aed3b0a5f75a7e3e823f6a4ad33a23a
Cherry <cherry@sparklet.org>
2024-07-20 15:18:39 -0400

    use vue (again)

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,9 +4,9 @@ import ViteExpress from "vite-express";
 const app = express();
 
 app.get("/hello", (_, res) => {
-    res.send("Hello Vite + TypeScript!");
+  res.send("Hello Vite + Vue + TypeScript!");
 });
 
 ViteExpress.listen(app, 3000, () =>
-    console.log("Server is listening on port 3000..."),
+  console.log("Server is listening on port 3000..."),
 );
