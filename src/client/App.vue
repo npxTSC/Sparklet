7581bf1baf9d42578ca359f0b1f1e8d59b725f81
Cherry <cherry@sparklet.org>
2024-07-20 15:26:59 -0400

    add back public folder stuff

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import TopBar from "./components/TopBar.vue";
 </script>
 
 <template>
     <div>
         <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
+            <img src="/img/vite.svg" class="logo" alt="Vite logo" />
         </a>
         <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+            <img src="/img/vue.svg" class="logo vue" alt="Vue logo" />
         </a>
     </div>
-    <HelloWorld msg="Vite + Vue" />
+    <TopBar msg="Vite + Vue" />
 </template>
 
 <style scoped>
