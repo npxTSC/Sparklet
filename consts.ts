08be781d66acb4500f6d36118bafb8ffd55cdadc
Cherry <cherry@sparklet.org>
2024-07-19 22:38:18 -0400

    simplify creation of anonymous account

--- consts.ts
+++ consts.ts
@@ -1,7 +1,5 @@
 "use strict";
 
-import { AdminRank } from "./classes.js";
-
 export const PORT = 3000;
 export const MAX_FILE_UPLOAD_MB = 200;
 export const QP_NAME_LIMIT = 20;
@@ -12,6 +10,3 @@ export const AUTH_TOKEN_BITS = 256;
 export const DEFAULT_BIO =
     "Hey! I'm a Sparklet user who didn't click this text and write a custom bio... yet!";
 
-export const ADMINS = {
-    "Anonymous": AdminRank.Conductor, // Reserved for default name
-};
