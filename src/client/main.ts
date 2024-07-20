57aff4ab3d6f276ad610e7e1bce0be53c291f3e0
Cherry <cherry@sparklet.org>
2024-07-20 15:45:38 -0400

    got bootstrap working

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.scss";
 
 import { createApp } from "vue";
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     root: "src/client",
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
