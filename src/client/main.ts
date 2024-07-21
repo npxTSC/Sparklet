03e39ee61dbef2e009c05274c218f0125688d7c3
Cherry <cherry@sparklet.org>
2024-07-21 11:41:45 -0400

    head works without javascript

--- src/client/main.ts
+++ src/client/main.ts
@@ -4,21 +4,4 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 
-document.head.innerHTML += `
-    <meta charset="UTF-8" />
-
-    <title>Sparklet | Check it out!</title>
-    <meta name="description" content="Experimental website. Always broken, never to be fully fixed! :D" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <link rel="icon" type="image/png" href="/img/favicon.png?v=1" />
-    <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.png?" />
-    <link rel="apple-touch-icon" href="/img/favicon.png?" />
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"
-        defer></script>
-    `
-
-
 createApp(App).mount("#app");
--- src/server/api.ts
+++ src/server/api.ts
@@ -6,7 +6,7 @@ const CHICKEN_WINGS_URL = "https://sparklet.org/img/tea-cape.png";
 router.get("/tea-capes", (_, res) => {
     // TODO admin portal for changing capes
     res.json({
-        "Mira_Serenade": CHICKEN_WINGS_URL,
+        "Cherry_Flanger": CHICKEN_WINGS_URL,
         "4772c57f-ca43-440c-be84-d5a97b676792": CHICKEN_WINGS_URL,
 
         "Epsilon094": CHICKEN_WINGS_URL,
@@ -16,5 +16,10 @@ router.get("/tea-capes", (_, res) => {
         "8d5fdde6-9ae9-480b-9cf2-30167933a10e": CHICKEN_WINGS_URL,
     });
 });
+
+router.get("/accounts/:id", (req, res) => {
+    //
+});
+
 
 export default router;
