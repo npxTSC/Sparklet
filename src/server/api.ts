dc9780e4aec024ef7c9788196fcad940fd8a5530
Cherry <cherry@sparklet.org>
2024-07-21 11:11:48 -0400

    tea capes api added

---
+++ src/server/api.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+const router = Router();
+
+const CHICKEN_WINGS_URL = "https://sparklet.org/img/tea-cape.png";
+
+router.get("/tea-capes", (_, res) => {
+    // TODO admin portal for changing capes
+    res.json({
+        "Mira_Serenade": CHICKEN_WINGS_URL,
+        "4772c57f-ca43-440c-be84-d5a97b676792": CHICKEN_WINGS_URL,
+
+        "Epsilon094": CHICKEN_WINGS_URL,
+        "8a7809c5-8351-4322-8f3f-6b5abce09c81": CHICKEN_WINGS_URL,
+
+        "III_zP0_III": CHICKEN_WINGS_URL,
+        "8d5fdde6-9ae9-480b-9cf2-30167933a10e": CHICKEN_WINGS_URL,
+    });
+});
+
+export default router;
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
+import api from "./api.js";
+
 const app = express();
 
 app.get("/hello", (_, res) => {
-  res.send("Hello Vite + Vue + TypeScript!");
+    res.send("Hello Vite + Vue + TypeScript!");
 });
 
+// app.use("/.well-known", routes.wk);
+app.use("/api", api);
+
+
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+    console.log("Server is listening on port 3000..."),
 );
