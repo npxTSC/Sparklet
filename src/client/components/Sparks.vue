3663e59665e8cbcb44c4856fca9cad05eb5fb59c
Cherry <cherry@sparklet.org>
2024-09-02 22:04:43 -0400

    render sparks list

--- src/client/components/Sparks.vue
+++ src/client/components/Sparks.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import TopBar from "./TopBar.vue";
+
+let sparks = ref([{ title: "Loading...", creatorName: "Loading...", date: 0, uuid: "Loading..." }])
+
+fetch("/api/sparks")
+    .then((response) => response.json())
+    .then((data) => {
+        sparks.value = data;
+    });
 
 defineProps<{ account: any }>();
 </script>
@@ -13,18 +22,15 @@ defineProps<{ account: any }>();
     </div>
 
     <div class="bb1">
-        <!-- <% for (qpost of qposts) { %> -->
-        <!-- <% date=new Date(qpost.date * 1000) %> -->
-        <!-- <a href="/sparks/<%= qpost.uuid %>" class="nodec"> -->
-        <!--     <div class="qpostBox"> -->
-        <!--         <h2 class="fst-italic text-white">"<%= qpost.title %>"</h2> -->
-        <!--         <h3 class="fst-italic text-white"> -->
-        <!--             from <%= qpost.creatorName %><br /><br /> -->
-        <!--                 Posted on <%= date.toLocaleDateString() %> at <%= date.toLocaleTimeString(undefined, -->
-        <!--                         {timeZone: "America/New_York" }) %> -->
-        <!--         </h3> -->
-        <!--     </div> -->
-        <!-- </a> -->
-        <!-- <% } %> -->
+        <a v-for="spark in sparks" :key="spark.uuid" :href="'/sparks/' + spark.uuid" class="nodec">
+            <div class="qpostBox">
+                <h2 class="fst-italic text-white">{{ spark.title }}</h2>
+                <h3 class="fst-italic text-white">
+                    from {{ spark.creatorName }}<br /><br />
+                    Posted on {{ new Date(spark.date * 1000).toLocaleDateString() }} at {{ new Date(spark.date *
+                        1000).toLocaleTimeString(undefined, { timeZone: "America/New_York" }) }}
+                </h3>
+            </div>
+        </a>
     </div>
 </template>
