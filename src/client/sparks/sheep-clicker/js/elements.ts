d61a5971158bfb5a80b21a4710ac97294e6bc28d
Cherry <cherry@sparklet.org>
2025-02-16 03:56:59 -0500

    fmt

--- src/client/sparks/sheep-clicker/js/elements.ts
+++ src/client/sparks/sheep-clicker/js/elements.ts
@@ -1,29 +1,29 @@
 namespace elements {
-	export const [
-		woolBags,
-		wbps,
-		wbpc,
-		clickSound,
-		music,
-		clickspace,
-		clickarea,
-		hrDepartment,
-		rdDepartment,
-		sheep,
-		sheepShakeContainer,
-	] = [
-		document.getElementById("pts"),
-		document.getElementById("wbps"),
-		document.getElementById("wbpc"),
-		(document.getElementById("cs") as HTMLAudioElement),
-		(document.getElementById("mus") as HTMLAudioElement),
-		document.getElementById("clickspace"),
-		(document.getElementById("clickarea") as HTMLDivElement),
-		document.getElementById("hr-department"),
-		document.getElementById("rd-department"),
-		document.getElementById("sheep"),
-		document.getElementById("sheep-shake-container"),
-	];
+    export const [
+        woolBags,
+        wbps,
+        wbpc,
+        clickSound,
+        music,
+        clickspace,
+        clickarea,
+        hrDepartment,
+        rdDepartment,
+        sheep,
+        sheepShakeContainer,
+    ] = [
+            document.getElementById("pts"),
+            document.getElementById("wbps"),
+            document.getElementById("wbpc"),
+            (document.getElementById("cs") as HTMLAudioElement),
+            (document.getElementById("mus") as HTMLAudioElement),
+            document.getElementById("clickspace"),
+            (document.getElementById("clickarea") as HTMLDivElement),
+            document.getElementById("hr-department"),
+            document.getElementById("rd-department"),
+            document.getElementById("sheep"),
+            document.getElementById("sheep-shake-container"),
+        ];
 }
 
 export default elements;
--- src/client/sparks/sheep-clicker/js/staticConf.ts
+++ src/client/sparks/sheep-clicker/js/staticConf.ts
@@ -1,14 +1,14 @@
-import Decimal	from "./_decimal";
-import {qstr}	from "./dx";
+import Decimal from "./_decimal";
+import { qstr } from "./dx";
 
 export enum $Unit {
-	SHEPHERD,
-	SHEARER,
-	KNITTER,
-	BABYSITTER,
-	PIZZAGUY,
+    SHEPHERD,
+    SHEARER,
+    KNITTER,
+    BABYSITTER,
+    PIZZAGUY,
 }
 
-export const FPS		= 20;
-export const FPS_DELAY	= 1000 / FPS;
-export const dev		= qstr.parseQ()["dev"] === "1";
+export const FPS = 20;
+export const FPS_DELAY = 1000 / FPS;
+export const dev = qstr.parseQ()["dev"] === "1";
