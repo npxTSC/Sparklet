56f99b56341845785f2c9f495f735a1e7e364cd4
Cherry <cherry@sparklet.org>
2025-04-09 17:59:53 -0400

    rm portfolio and cafe navbar links (for now)

--- src/client/components/TopBar.vue
+++ src/client/components/TopBar.vue
@@ -31,10 +31,10 @@ const account = accountStore.account;
                         </ul>
                     </li>
                     <li class="nav-item"><a class="nav-link active text-black" href="/sparks/">Sparks</a></li>
-                    <li class="nav-item"><a class="nav-link active text-black" href="/portfolio/">Portfolio</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link active text-black" href="/portfolio/">Portfolio</a></li> -->
                     <li class="nav-item"><a class="nav-link active text-black" href="/about/">About</a></li>
-                    <li class="nav-item"><a class="nav-link active text-black"
-                            href="https://blog.sparklet.org/">Cafe</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link active text-black" -->
+                    <!--         href="https://blog.sparklet.org/">Cafe</a></li> -->
                     <li class="nav-item"><a class="nav-link active text-black"
                             href="https://pond.sparklet.org/">Pond</a></li>
                 </ul>
