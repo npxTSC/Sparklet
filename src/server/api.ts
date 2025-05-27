73ad6eda588b6e36e5236298f62d2fc6fb2082f9
Cherry <cherry@sparklet.org>
2025-05-27 00:53:54 -0400

    rm usernames for tea cape, only use uuid

--- src/server/api.ts
+++ src/server/api.ts
@@ -3,21 +3,21 @@ import { accounts } from "./api/accounts.js";
 
 import db from "./db.js";
 
-const CHICKEN_WINGS_URL = "https://sparklet.org/img/tea-cape.png";
+const TEA_CAPE_URL = "https://sparklet.org/img/tea-cape.png";
 
 const api = Router();
 api.use("/accounts", accounts);
 
 api.get("/tea-capes", (_, res) => { // TODO admin portal for changing capes
     res.json({
-        "Cherry_Flanger": CHICKEN_WINGS_URL,
-        "4772c57f-ca43-440c-be84-d5a97b676792": CHICKEN_WINGS_URL,
+        // Cherry
+        "4772c57f-ca43-440c-be84-d5a97b676792": TEA_CAPE_URL,
 
-        "Epsilon094": CHICKEN_WINGS_URL,
-        "8a7809c5-8351-4322-8f3f-6b5abce09c81": CHICKEN_WINGS_URL,
+        // Pepsi
+        "8a7809c5-8351-4322-8f3f-6b5abce09c81": TEA_CAPE_URL,
 
-        "III_zP0_III": CHICKEN_WINGS_URL,
-        "8d5fdde6-9ae9-480b-9cf2-30167933a10e": CHICKEN_WINGS_URL,
+        // fuck off
+        // "8d5fdde6-9ae9-480b-9cf2-30167933a10e": TEA_CAPE_URL,
     });
 });
 
